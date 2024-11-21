e2753fcb43e786645741b290100a173bbc81a3ce
kumanboy <husandavronov13@gmail.com>
2024-11-21 12:24:41 +0500

    Components End

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
+import Header from "./components/Header.jsx";
+import Main from "./components/Main.jsx";
+import Footer from "./components/Footer.jsx";
+
 export const App = () => {
     return (
-        <section>
-            Hello
-        </section>
+        <div>
+            <Header/>
+            <Main/>
+            <Footer/>
+        </div>
     )
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,8 @@
+const Footer = () => {
+    return (
+        <div>
+            <p>© 2024 Mening Vebsaytim </p>
+        </div>
+    )
+}
+export default Footer
