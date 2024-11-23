29ea23d5ba83c246000427ccee0b45e1d2380470
kumanboy <husandavronov13@gmail.com>
2024-11-23 10:12:37 +0500

    Styling end

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-import "./index.css"
-import { BiLogoTelegram } from "react-icons/bi";
 export const App = () => {
     return (
         <section>
-            <BiLogoTelegram />
+
         </section>
     )
 }
+
+
 
