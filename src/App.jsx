2fa32e9516d0db2d1526d504c5e28dbfe8a37bb5
kumanboy <husandavronov13@gmail.com>
2024-11-23 10:52:56 +0500

    Events end

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,23 @@
-export const App = () => {
-    return (
-        <section>
+const Move = () => {
+    function mouseMove(){
+        alert("MouseMove event ishladi")
+        console.log("MouseMove event ishladi")
+    }
 
-        </section>
+    return (
+        <p onMouseMove={mouseMove}>
+            viris purus graeco omittam adolescens movet maximus oporteat viverra efficiantur instructior sententiae
+            posidonium appareat quam pertinax sonet suas maximus alienum penatibus numquam unum congue posidonium
+            molestiae urna consectetur salutatus quidam
+        </p>
     )
 }
 
 
-
+export const App = () => {
+    return (
+        <section>
+            <Move/>
+        </section>
+    )
+}
