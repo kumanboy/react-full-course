d49b6d80689481cf2c3cedc3d0a563e081ff334b
kumanboy <husandavronov13@gmail.com>
2025-01-24 15:50:44 +0500

    useReducer hook end

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,30 @@
-import {UserProvider} from "./UserContext.jsx";
-import UserProfile from "./components/UserProfile.jsx";
-import UpdateUser from "./components/UpdateUser.jsx";
+import { useReducer } from 'react';
+
+const initialState = { count: 0 };
+
+const reducer = (state, action) => {
+    switch (action.type) {
+        case 'INCREMENT':
+            return { count: state.count + 1 };
+        case 'DECREMENT':
+            return { count: state.count - 1 };
+        case 'RESET':
+            return { count: 0 };
+        default:
+            throw new Error('Unknown action type');
+    }
+};
 
 export const App = () => {
+    const [state, dispatch] = useReducer(reducer, initialState);
 
-    return(
-     <div>
-        <UserProvider>
-            <UserProfile/>
-            <UpdateUser/>
-        </UserProvider>
-     </div>
- )
-}
+    return (
+        <div>
+            <h1>Count: {state.count}</h1>
+            <button onClick={() => dispatch({ type: 'INCREMENT' })}>Increment</button>
+            <button onClick={() => dispatch({ type: 'DECREMENT' })}>Decrement</button>
+            <button onClick={() => dispatch({ type: 'RESET' })}>Reset</button>
+        </div>
+    );
+};
 
