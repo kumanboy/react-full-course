b6b1f43edf5367a28b0f2db6939976ad35e20ba6
kumanboy <husandavronov13@gmail.com>
2025-01-31 10:19:05 +0500

    useReducer tasks done

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,9 @@
-import { useReducer } from 'react';
-
-const initialState = { count: 0 };
-
-const reducer = (state, action) => {
-    switch (action.type) {
-        case 'INCREMENT':
-            return { count: state.count + 1 };
-        case 'DECREMENT':
-            return { count: state.count - 1 };
-        case 'RESET':
-            return { count: 0 };
-        default:
-            throw new Error('Unknown action type');
-    }
-};
+import Counter from "./components/Counter.jsx";
 
 export const App = () => {
-    const [state, dispatch] = useReducer(reducer, initialState);
 
     return (
-        <div>
-            <h1>Count: {state.count}</h1>
-            <button onClick={() => dispatch({ type: 'INCREMENT' })}>Increment</button>
-            <button onClick={() => dispatch({ type: 'DECREMENT' })}>Decrement</button>
-            <button onClick={() => dispatch({ type: 'RESET' })}>Reset</button>
-        </div>
+        <Counter/>
     );
 };
 
