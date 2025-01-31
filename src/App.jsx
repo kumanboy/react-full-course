6f2ba60f8778f0ea9f8ebb913d161f26d49f67e1
kumanboy <husandavronov13@gmail.com>
2025-01-31 11:00:36 +0500

    useRef with prev state

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,20 @@
 import { useRef, useState, useEffect } from "react";
 
-export default function App() {
-    const countRef = useRef(0); // useRef – Renderga ta’sir qilmaydi
-    const [stateCount, setStateCount] = useState(0); // useState bilan
+export default function App()  {
+    const [count, setCount] = useState(0);
+    const prevCountRef = useRef(null);
 
-    // Har safar komponent  (re-render bo‘lganda) ekranga xabar chiqaramiz
     useEffect(() => {
-        console.log("Component Re-rendered!");
-    });
-
-    const incrementRef = () => {
-        countRef.current += 1;
-        console.log("Ref count:", countRef.current); // Faqat konsolda o‘zgaradi
-    };
-
-    const incrementState = () => {
-        setStateCount(stateCount + 1); // Qayta render qiladi
-    };
+        prevCountRef.current = count; // Har safar `count` o'zgarganda eski qiymatni saqlaydi
+    }, [count]);
 
     return (
         <div>
-            {console.log("Rendering...")} {/* Qayta render bo‘lganda chiqadi */}
-            <p>State Count: {stateCount}</p>
-            <p>Ref Count (Ko‘rinmaydi): {countRef.current}</p>
-            <button onClick={incrementState}>Increase State</button>
-            <button onClick={incrementRef}>Increase Ref</button>
+            <p>Current Count: {count}</p>
+            <p>Previous Count: {prevCountRef.current}</p>
+            <button onClick={() => setCount(count + 1)}>Increase</button>
         </div>
     );
-}
+};
+
+
